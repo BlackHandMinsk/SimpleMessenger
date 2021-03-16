7a2c6b383da72041f0c7f84a4e4e2a3754e19656
BlackHandMinsk <3058079@gmail.com>
2021-03-16 13:59:08 +0300

    some changes

--- app/src/main/java/com/example/simplemessenger/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/simplemessenger/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,8 @@
 package com.example.simplemessenger.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.simplemessenger.R
 import com.example.simplemessenger.databinding.FragmentChatsBinding
 import com.example.simplemessenger.databinding.FragmentSettingsBinding
@@ -14,6 +12,10 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+       activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
     }
 }
