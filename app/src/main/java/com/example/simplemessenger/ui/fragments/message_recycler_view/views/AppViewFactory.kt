2778b04ce4d95e3f45c6f725b3a9d4c94163cc42
BlackHandMinsk <3058079@gmail.com>
2021-04-13 09:47:52 +0300

    voice messages

--- app/src/main/java/com/example/simplemessenger/ui/fragments/message_recycler_view/views/AppViewFactory.kt
+++ app/src/main/java/com/example/simplemessenger/ui/fragments/message_recycler_view/views/AppViewFactory.kt
@@ -3,12 +3,14 @@ package com.example.simplemessenger.ui.fragments.message_recycler_view.views
 import android.view.View
 import com.example.simplemessenger.models.CommonModel
 import com.example.simplemessenger.utilits.TYPE_MESSAGE_IMAGE
+import com.example.simplemessenger.utilits.TYPE_MESSAGE_VOICE
 
 class AppViewFactory {
     companion object{
         fun getView(message:CommonModel):MessageView{
             return when(message.type){
                 TYPE_MESSAGE_IMAGE->ViewImageMessage(message.id,message.from,message.timeStamp.toString(),message.fileUrl)
+                TYPE_MESSAGE_VOICE->ViewVoiceMessage(message.id,message.from,message.timeStamp.toString(),message.fileUrl)
                 else->ViewTextMessage(message.id,message.from,message.timeStamp.toString(),message.fileUrl,message.text)
             }
         }
