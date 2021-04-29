addddcc4e6b8ac74c1e3896219d973edb285fea8
BlackHandMinsk <3058079@gmail.com>
2021-04-29 13:12:03 +0300

    group chat

--- app/src/main/java/com/example/simplemessenger/ui/screens/groups/CreateGroupFragment.kt
+++ app/src/main/java/com/example/simplemessenger/ui/screens/groups/CreateGroupFragment.kt
@@ -1,26 +1,54 @@
 package com.example.simplemessenger.ui.screens.groups
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.simplemessenger.R
 import com.example.simplemessenger.database.*
 import com.example.simplemessenger.models.CommonModel
 import com.example.simplemessenger.ui.screens.base.BaseFragment
+import com.example.simplemessenger.ui.screens.main_list.MainListFragment
 import com.example.simplemessenger.utilits.*
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.fragment_add_contacts.*
 import kotlinx.android.synthetic.main.fragment_create_group.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class CreateGroupFragment(private var listContacts:List<CommonModel>):BaseFragment(R.layout.fragment_create_group) {
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var mAdapter:AddContactsAdapter
+    private var mUri = Uri.EMPTY
 
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title = "Создать группу"
         hideKeybord()
         initRecycleView()
-        create_group_btn_complete.setOnClickListener{ showToast("ok") }
+        create_group_photo.setOnClickListener{addPhoto()}
+        create_group_btn_complete.setOnClickListener{
+            val nameGroup = create_group_input_name.text.toString()
+            if(nameGroup.isEmpty()){
+                showToast("Введите имя")
+            }else{
+                createGroupToDatabase(nameGroup,mUri,listContacts){
+                    replaceFragment(MainListFragment())
+                }
+            }
+        }
         create_group_input_name.requestFocus()
         create_group_counts.text = getPlurals(listContacts.size)
+    }
+
+
+
+    private fun addPhoto() {
+        CropImage.activity()
+                .setAspectRatio(1,1)
+                .setRequestedSize(600,600)
+                .setCropShape(CropImageView.CropShape.OVAL)
+                .start(APP_ACTIVITY,this)
     }
 
     private fun initRecycleView() {
@@ -28,5 +56,13 @@ class CreateGroupFragment(private var listContacts:List<CommonModel>):BaseFragme
         mAdapter = AddContactsAdapter()
         mRecyclerView.adapter = mAdapter
         listContacts.forEach{ mAdapter.updateListItems(it)}
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE&&resultCode== Activity.RESULT_OK &&data!=null){
+            mUri = CropImage.getActivityResult(data).uri
+            create_group_photo.setImageURI(mUri)
+        }
     }
 }
